b487c965355f21f802b35a92c03b4f99043b3327
Dominic Lee <dominictwlee@gmail.com>
2023-03-07 23:09:01 +0000

    add large flat list

--- App.tsx
+++ App.tsx
@@ -8,27 +8,87 @@
  * @format
  */
 
-import React from 'react';
-import { Text, View, Button } from 'react-native';
+import React, { memo, useCallback, useMemo } from 'react';
+import {
+  Text,
+  View,
+  Button,
+  FlatList,
+  ListRenderItem,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import { nanoid } from 'nanoid/non-secure';
+import Animated, { SlideInLeft, SlideOutRight } from 'react-native-reanimated';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { faker } from '@faker-js/faker';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 const NativeStack = createNativeStackNavigator();
 
+const items = Array.from({ length: 2000 }, () => ({
+  id: nanoid(),
+  title: faker.name.firstName(),
+}));
+
 function HomeScreenA() {
   const { navigate } = useNavigation();
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const data = useMemo(() => {
+    return items.filter(item => item.title.includes(searchTerm));
+  }, [searchTerm]);
+
+  const renderItem = useCallback<
+    ListRenderItem<{
+      title: string;
+    }>
+  >(({ item }) => {
+    return <ListItem title={item.title} />;
+  }, []);
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home A</Text>
-      <Button title="Go to Home B" onPress={() => navigate('HomeB')} />
+    <View style={{ flex: 1 }}>
+      <View style={{ justifyContent: 'flex-end', padding: 20 }}>
+        <Button title="Go to Home B" onPress={() => navigate('HomeB')} />
+        <TextInput
+          value={searchTerm}
+          onChangeText={setSearchTerm}
+          placeholder="search..."
+        />
+      </View>
+      <FlatList
+        contentContainerStyle={styles.listContent}
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={extractKey}
+        ItemSeparatorComponent={Separator}
+      />
     </View>
   );
 }
+
+function extractKey(item: { id: string; title: string }) {
+  return item.id;
+}
+
+function Separator() {
+  return <View style={{ height: 16 }} />;
+}
+
+const ListItem = memo((props: { title: string }) => {
+  return (
+    <Animated.View
+      entering={SlideInLeft.randomDelay(1000)}
+      exiting={SlideOutRight}
+      style={styles.listItemCard}>
+      <Text style={styles.listItemTitle}>{props.title}</Text>
+    </Animated.View>
+  );
+});
 
 function HomeScreenB() {
   return (
@@ -85,5 +145,27 @@ const App = () => {
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 16,
+  },
+  listContent: {
+    paddingHorizontal: 16,
+  },
+  listItemCard: {
+    flexDirection: 'row',
+    height: 60,
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  listItemTitle: {
+    fontSize: 20,
+  },
+});
 
 export default App;
