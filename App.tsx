06d1dd552b08439984dbf767fa23b355d9434e48
Dominic Lee <dominictwlee@gmail.com>
2023-03-07 21:20:03 +0000

    initial commit

---
+++ App.tsx
@@ -0,0 +1,84 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+const NativeStack = createNativeStackNavigator();
+
+function HomeScreenA() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home A</Text>
+    </View>
+  );
+}
+
+function HomeScreenB() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home B</Text>
+    </View>
+  );
+}
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeA" component={HomeScreenA} />
+      <Stack.Screen name="HomeB" component={HomeScreenB} />
+    </Stack.Navigator>
+  );
+}
+
+function SettingsScreenA() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings A</Text>
+    </View>
+  );
+}
+
+function SettingsScreenB() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings B</Text>
+    </View>
+  );
+}
+
+function SettingsStack() {
+  return (
+    <NativeStack.Navigator>
+      <NativeStack.Screen name="SettingsA" component={SettingsScreenA} />
+      <NativeStack.Screen name="SettingsB" component={SettingsScreenB} />
+    </NativeStack.Navigator>
+  );
+}
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Settings" component={SettingsStack} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
