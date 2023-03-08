7291bcb0cf1d9baa1d35ca16cc0972818fdec60c
Dominic Lee <dominictwlee@gmail.com>
2023-03-08 09:35:39 +0000

    add types

--- types.ts
+++ types.ts
@@ -1,3 +1,5 @@
+import { NavigatorScreenParams } from '@react-navigation/native';
+
 declare global {
   namespace ReactNavigation {
     interface RootParamList extends RootStackParamList {}
@@ -5,8 +7,13 @@ declare global {
 }
 
 export type RootStackParamList = {
+  Home: NavigatorScreenParams<{ HomeA: undefined; HomeB: undefined }>;
   HomeA: undefined;
   HomeB: undefined;
+  Settings: NavigatorScreenParams<{
+    SettingsA: undefined;
+    SettingsB: undefined;
+  }>;
   SettingsA: undefined;
   SettingsB: undefined;
 };
